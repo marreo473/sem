8bf291c8cf82db4f551d9f20095dc7bff5ecfdc8
marreo473 <98224222+marreo473@users.noreply.github.com>
2022-02-06 20:38:27 -0400

    test run

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,7 +4,28 @@ import java.sql.*;
 
 public class App
 {
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
     public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
+
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    public void connect()
     {
         try
         {
@@ -17,9 +38,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -30,9 +49,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +61,68 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
 
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+                    String strSelect =
+                 "select e.emp_no, e.first_name,e.last_name,t.title,s.salary,"
+
+                        +"d.dept_name,e2.first_name as manager_first_name,e2.last_name as manager_last_name"
+                        + " From employees e inner join salaries s on e.emp_no = s.emp_no "
+                        + " Inner join titles t on t.emp_no = e.emp_no "
+                        + " Inner join dept_emp de on de.emp_no = e.emp_no "
+                        + " Inner join departments d on d.dept_no = de.dept_no "
+                        + " Inner join dept_manager dm on dm.dept_no = de.dept_no "
+                        + " Inner join employees e2 on e2.emp_no = de.emp_no "
+                        + " Where e.emp_no = " + ID;
+
+                    // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
